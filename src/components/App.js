f9d3f6c3f307b400910995e54e32d96abc939078
Andrew James <ajames20@gmail.com>
2018-07-18 14:26:07 -0600

    Init commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const App = () => {
+  return (
+    <div>
+      <h2>hello workd</h2>
+    </div>
+  );
+};
+
+App.propTypes = {
+
+};
+
+export default App;
