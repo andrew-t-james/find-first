1264b575348b8ca0b6d7e6bc81231410d360cd12
Andrew James <ajames20@gmail.com>
2018-07-19 18:09:54 -0600

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,5 +2,9 @@ import DotEnv from 'dotenv';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-configure({ adapter: new Adapter() });
+configure({
+  adapter: new Adapter(),
+  disableLifecycleMethods: true
+});
+
 DotEnv.config({ path: '.env.test' });
