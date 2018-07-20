b5a6fe3a2bc78a93de411800dafecdfcf7dd92d6
Andrew James <ajames20@gmail.com>
2018-07-20 14:05:25 -0600

    Update router

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import  SignIn from '../SignIn/SignIn';
+import SignIn from '../SignIn/SignIn';
+import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const Home = () => {
   return (
     <section>
       <SignIn />
+      <p>Dont have and account ? <NavLink to="/sign-up" >Sign Up</NavLink></p>
     </section>
   );
 };
--- src/router/Router.js
+++ src/router/Router.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from '../Components/Home/Home';
 import Dashboard from '../Components/Dashboard/Dashboard';
+import SignUp from '../Components/SignUp/SignUp';
 
 const AppRouter = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/dashboard" exact component={Dashboard} />
+      <Route path="/sign-up" exact component={SignUp} />
     </Switch>
   </BrowserRouter>
 );
