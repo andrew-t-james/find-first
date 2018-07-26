646c9e3711e5f7da4d3b55dba3d4a7001e3deaf4
Andrew James <ajames20@gmail.com>
2018-07-26 05:13:16 -0600

    Add test coverage for filtering jobs

--- src/Components/Dashboard/__test__/Dashboard.test.js
+++ src/Components/Dashboard/__test__/Dashboard.test.js
@@ -24,6 +24,56 @@ describe('<Dashboard />', () => {
     expect(mockGithubJobs).toHaveBeenCalled();
   });
 
+  describe('handleUpdate', () => {
+    test('should update state onChange', () => {
+      const mockState = {
+        query: ''
+      };
+      const mockEvent = {
+        preventDefault: jest.fn(),
+        target: {
+          name: 'query',
+          value: 'my hot job'
+        }
+      };
+
+      wrapper.instance().handleUpdate(mockEvent);
+      expect(wrapper.state('query')).toBe('my hot job');
+    });
+  });
+
+  describe('filterJobs', () => {
+    test('should filter jobs caseInsensitive', () => {
+      const mockJobs = [
+        {
+          title: 'Front End',
+          id:1
+        },
+        {
+          title: 'VP Engineering',
+          id: 2
+        }
+      ];
+      const expected = [
+        {
+          title: 'Front End',
+          id:1
+        }
+      ];
+
+      wrapper = shallow(
+        <Dashboard
+          jobs={mockJobs}
+          githubJobs={mockGithubJobs}
+        />);
+
+      wrapper.instance().filterJobs('front');
+      expect(wrapper.state('jobs')).toEqual(expected);
+    });
+
+  });
+
+
   describe('mapStateToProps', () => {
     test('should map githubJobs to props', () => {
       const mockState = {
