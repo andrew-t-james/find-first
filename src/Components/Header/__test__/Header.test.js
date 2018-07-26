cc484c80a7803f528625d09c15c8cef8a012789d
Andrew James <ajames20@gmail.com>
2018-07-26 08:10:18 -0600

    Add user info to ui and test coveraged

--- src/Components/Header/__test__/Header.test.js
+++ src/Components/Header/__test__/Header.test.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { Header } from '../Header';
+import { Header, mapStateToProps } from '../Header';
 
 describe('<Header />', () => {
   let wrapper;
+  const mockUser = {
+    name: 'Joben',
+    image: 'some-url-here'
+  };
 
-  beforeEach(() => wrapper = shallow(<Header />));
+  beforeEach(() => wrapper = shallow(<Header user={mockUser}/>));
 
   test('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  describe('mapStateToProps', () => {
+    const expected = {
+      user: mockUser
+    };
+    const mockState = {
+      user: mockUser
+    };
+    const result = mapStateToProps(mockState);
+    expect(result).toEqual(expected);
+  });
+
 });
