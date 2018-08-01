3d66d8cad038e80bc9a7301a47279f2363332532
Andrew James <ajames20@gmail.com>
2018-08-01 07:51:53 -0600

    Add condition loginc for user sigining in

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Router, { history } from './router/Router';
 import { Provider } from 'react-redux';
 import { firebase } from './firebase/firebase';
 import configureStore from './store/configureStore';
-import { googleSignInAction, twitterLoginAction, githubLoginAction, facebookLoginAction } from './Actions/auth';
+import { googleSignInAction, twitterLoginAction, githubLoginAction, facebookLoginAction, googleSignOutAction } from './Actions/auth';
 
 const store = configureStore();
 
@@ -16,26 +16,42 @@ const app = (
 );
 
 firebase.auth().onAuthStateChanged(async user => {
+  if (!user) {
+    history.push('/');
+    console.log('log out');
+    renderApp();
+  }
   const provider = user.providerData[0].providerId;
 
   if (provider.includes('github')) {
     await store.dispatch(githubLoginAction(user));
     history.push('/dashboard');
+    renderApp();
   } else if (provider.includes('google')) {
     await store.dispatch(googleSignInAction(user));
     history.push('/dashboard');
+    renderApp();
   } else if (provider.includes('twitter')) {
     await store.dispatch(twitterLoginAction(user));
     history.push('/dashboard');
+    renderApp();
   } else if (provider.includes('facebook')) {
     await store.dispatch(facebookLoginAction(user));
     history.push('/dashboard');
-  } else {
-    history.push('/');
-    console.log('log out');
+    renderApp();
   }
+
 });
 
-ReactDOM.render(app, document.getElementById('app'));
+const renderApp = () => {
+  let hasRendered = false;
+  if (!hasRendered) {
+    ReactDOM.render(app, document.getElementById('app'));
+    hasRendered = true;
+  } else {
+    ReactDOM.render(app, document.getElementById('app'));
+  }
+};
+
 
 
